b5a2d2615043d0771baa6eee1b5a745d664a98bb
Alex Long <shiversandmilk@gmail.com>
2015-08-22 00:52:54 -0400

    App prototype successful!
    This includes the "totem color" to let you know that you didn't mispell
    your secret phrase.

--- src/com/hashtest/MainActivity.java
+++ src/com/hashtest/MainActivity.java
@@ -1,40 +1,76 @@
-package com.hashtest;
+package com.pashword;
 
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.app.Activity;
 import android.os.Bundle;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import android.util.Base64;
+import java.util.ArrayList;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.View;
+import android.text.TextWatcher;
+import android.text.Editable;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 
 public class MainActivity extends Activity
 {
-    /** Called when the activity is first created. */
+    EditText message_tv, secret_tv;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        Toast.makeText(this, "HELLO!", Toast.LENGTH_LONG).show();
-        System.out.println("ALEXLONG123 - START");
+        message_tv = (EditText) findViewById(R.id.message);
+        secret_tv = (EditText) findViewById(R.id.secret);
+
+        secret_tv.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+                String secret = secret_tv.getText().toString();
+                try {
+                    byte[] bytes = generate_hash("Pashword".getBytes(), secret);
+                    int color_int = Color.parseColor("#" + bytesToHex(bytes).substring(0, 6));
+                    ((TextView) findViewById(R.id.color_int)).setText("Color int: " + color_int);
+                    View totem_view = findViewById(R.id.totem);
+                    GradientDrawable totem_drawable = (GradientDrawable)totem_view.getBackground();
+                    totem_drawable.setColor(color_int);
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "error: " + e, Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+       });
+    }
+
+    public void generate_and_show_hashes(View v) throws Exception {
         try {
-            String secret = "test";
-            String message = "test";
+            String message = message_tv.getText().toString();
+            String secret = secret_tv.getText().toString();
+            String hash = "";
 
-            byte[] bytes = hash(message.getBytes(), secret);
-            String hash = Base64.encodeToString(bytes, Base64.DEFAULT);
-            String output = "";
-            output = output + "base64 of hash: " + hash + "\n";
-            output = output + "hex of hash: " + bytesToHex(bytes) + "\n";
+            ArrayList<String> hashes = new ArrayList<String>();
+            byte[] bytes = generate_hash(message.getBytes(), secret);
 
-            bytes = hash(bytes, secret);
-            hash = Base64.encodeToString(bytes, Base64.DEFAULT);
-            output = output + "base64 of 2nd hash: " + hash + "\n";
-            output = output + "hex of 2nd hash: " + bytesToHex(bytes) + "\n";
-            output = output + "ALEXLONG123" + "\n";
-            ((TextView) findViewById(R.id.text_view)).setText(output);
-            Toast.makeText(this, "hash is: " + hash, Toast.LENGTH_LONG).show();
+            for (int i = 0; i < 10; i++) {
+                bytes = generate_hash(bytes, secret);
+                hash = Base64.encodeToString(bytes, Base64.DEFAULT);
+                hashes.add(hash.substring(0, 10));
+            }
+
+            ArrayAdapter<String> hashListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, hashes);
+            ListView hashListView = (ListView)findViewById(R.id.hashListView);
+            hashListView.setAdapter(hashListAdapter);
         } catch (Exception e) {
             Toast.makeText(this, "error: " + e, Toast.LENGTH_LONG).show();
             System.out.println("ALEXLONG123 - error");
@@ -44,7 +80,7 @@ public class MainActivity extends Activity
         }
     }
 
-    public static byte[] hash(byte[] message, String key) throws Exception {
+    public static byte[] generate_hash(byte[] message, String key) throws Exception {
         Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
         SecretKeySpec secret_key = new SecretKeySpec(key.getBytes(), "HmacSHA256");
         sha256_HMAC.init(secret_key);
@@ -62,4 +98,14 @@ public class MainActivity extends Activity
         }
         return new String(hexChars);
     }
+
+    public static int byteArrayToInt(byte[] b) {
+        int value = 0;
+        for (int i = 0; i < 4; i++) {
+            int shift = (4 - 1 - i) * 8;
+            value += (b[i] & 0x000000FF) << shift;
+        }
+        return value;
+    }
+
 }
